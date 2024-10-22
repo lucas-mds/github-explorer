42fcd59b3d64302de213895b9f7e3394823864e8
João Lucas Moreira da Silva <aklusdsi@gmail.com>
2024-10-23 00:25:34 +0200

    Updating search users API url

--- src/hooks/use-search-users/use-search-users.tsx
+++ src/hooks/use-search-users/use-search-users.tsx
@@ -4,10 +4,11 @@ import { useQuery } from "@tanstack/react-query";
 const octokit = new Octokit();
 
 const fetchUsers = async (query: string) => {
-  const data = await octokit.request("GET /users", {
+  const data = await octokit.request("GET /search/users", {
     headers: {
       "X-GitHub-Api-Version": "2022-11-28",
     },
+    q: query,
   });
 
   return data;
