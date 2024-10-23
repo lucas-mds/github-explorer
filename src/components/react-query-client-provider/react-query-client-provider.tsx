fcdb9b429093fff6a4c566addea1ec213027bcea
João Lucas Moreira da Silva <aklusdsi@gmail.com>
2024-10-23 17:55:59 +0200

    Preventing unnecessary refetchs on window focus

--- src/components/react-query-client-provider/react-query-client-provider.tsx
+++ src/components/react-query-client-provider/react-query-client-provider.tsx
@@ -7,7 +7,16 @@ const ReactQueryClientProvider = ({
 }: {
   children: React.ReactNode;
 }) => {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+  );
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
